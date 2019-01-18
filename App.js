3c49cecf0a51325be3e617250a9bf905209079e0
maykon <maykonmichel@gitlab.com>
2019-01-18 11:14:10 -0200

    npm version 0.1.1 Removed Sentry for incompatibility with template (@Todo: find a way)

--- App.js
+++ App.js
@@ -2,12 +2,9 @@ import React from 'react';
 import { StatusBar, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { Sentry } from 'react-native-sentry';
 
 import Navigator from './src/navigation';
 import { persistor, store } from './src/store';
-
-Sentry.config('dns').install();
 
 export default () => (
   <View style={{ flex: 1 }}>
