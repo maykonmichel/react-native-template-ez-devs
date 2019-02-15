fe20f63086602a31e180eb93a4d1103b9e21921a
maykon <maykonmichel@gitlab.com>
2019-02-15 13:27:01 -0200

    npm version 0.5.1

--- android/app/src/main/java/com/helloworld/MainApplication.java
+++ android/app/src/main/java/com/helloworld/MainApplication.java
@@ -26,9 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RNGestureHandlerPackage()
+          new MainReactPackage()
       );
     }
 
