3c89d21967fc6dd133ae637fe1fcdb858335e3bd
maykon <maykonmichel@gitlab.com>
2019-02-19 10:53:31 -0300

    Added jsx-control-statements

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,34 @@
 module.exports = {
-  'extends': ['airbnb', 'prettier'],
-  'plugins': ['prettier'],
+  'plugins': [
+    'prettier',
+    'jsx-control-statements'
+  ],
+  'extends': [
+    'airbnb',
+    'prettier',
+    'plugin:jsx-control-statements/recommended'
+  ],
   'parser': 'babel-eslint',
   'env': {
     'jest': true,
+    'jsx-control-statements/jsx-control-statements': true
   },
   'rules': {
     'no-use-before-define': 'off',
     'react/jsx-filename-extension': 'off',
     'react/prop-types': 'off',
     'comma-dangle': 'off',
-    'prettier/prettier': ['error']
+    'prettier/prettier': ['error'],
+    'react/jsx-no-undef': [2, { 'allowGlobals': true }],
+    'jsx-control-statements/jsx-choose-not-empty': 1,
+    'jsx-control-statements/jsx-for-require-each': 1,
+    'jsx-control-statements/jsx-for-require-of': 1,
+    'jsx-control-statements/jsx-if-require-condition': 1,
+    'jsx-control-statements/jsx-otherwise-once-last': 1,
+    'jsx-control-statements/jsx-use-if-tag': 1,
+    'jsx-control-statements/jsx-when-require-condition': 1,
+    'jsx-control-statements/jsx-jcs-no-undef': 1,
+    'no-undef': 0
   },
   'globals': {
     'fetch': false
