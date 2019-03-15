ded0c6a2a3c8490b9304f25270c4059956532108
maykon <maykonmichelpalma@gmail.com>
2019-03-15 18:27:15 -0300

    Bug fixed react native update

--- android/app/src/main/java/com/helloworld/MainApplication.java
+++ android/app/src/main/java/com/helloworld/MainApplication.java
@@ -5,8 +5,6 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
