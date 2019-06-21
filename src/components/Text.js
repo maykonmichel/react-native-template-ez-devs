c47a3e382a2945cd94ca6664f31b07dfa07b625d
Maykon deepin <maykonmichelpalma@gmail.com>
2019-06-21 13:43:47 -0300

    Updated text component

---
+++ src/components/Text.js
@@ -0,0 +1,28 @@
+import React, { useMemo } from 'react';
+import { StyleSheet, Text as RNText } from 'react-native';
+import PropTypes from 'prop-types';
+
+import typographies, { typographiesKeys } from '../configs/typographies';
+
+const Text = ({ children, typography, style, ...attributes }) => {
+  const typographyStyle = useMemo(() => typographies[typography], [typography]);
+
+  return (
+    <RNText style={StyleSheet.flatten([typographyStyle, style])} {...attributes}>
+      {children}
+    </RNText>
+  );
+};
+
+Text.propTypes = {
+  ...RNText.propTypes,
+  style: RNText.propTypes.style,
+  typography: PropTypes.oneOf(typographiesKeys)
+};
+
+Text.defaultProps = {
+  style: {},
+  typography: 'headline'
+};
+
+export default React.memo(Text);
